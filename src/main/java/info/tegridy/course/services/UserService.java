975f0254599f1d2ca3ba31367fcf218be0907a1a
Fernando Duarte <duarte.brusque@gmail.com>
2020-06-08 23:25:03 -0300

    Service layer, component registration

---
+++ src/main/java/info/tegridy/course/services/UserService.java
@@ -0,0 +1,26 @@
+package info.tegridy.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import info.tegridy.course.entities.User;
+import info.tegridy.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}
