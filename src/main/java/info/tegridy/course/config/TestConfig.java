fa728fe6d87fad202ccaafa04fe86534f211a423
Fernando Duarte <duarte.brusque@gmail.com>
2020-06-10 12:03:36 -0300

    Payment, one-to-one association

--- src/main/java/info/tegridy/course/config/TestConfig.java
+++ src/main/java/info/tegridy/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import info.tegridy.course.entities.Category;
 import info.tegridy.course.entities.Order;
 import info.tegridy.course.entities.OrderItem;
+import info.tegridy.course.entities.Payment;
 import info.tegridy.course.entities.Product;
 import info.tegridy.course.entities.User;
 import info.tegridy.course.entities.enums.OrderStatus;
@@ -80,5 +81,10 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
